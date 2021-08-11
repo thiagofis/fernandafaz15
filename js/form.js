251b2d12baaeb05b8b0e5b6839e91ae51593022a
Thiago Marcolino <thiago.marcolino@thetrainline.com>
2021-08-11 21:58:16 +0100

    test form

--- js/form.js
+++ js/form.js
@@ -1,12 +1,18 @@
 $("#form-submit").click(function () {
     console.log("1")
     $.ajax({
-        url: "https://fernanda15func.azurewebsites.net/api/PostRequestFunction",
+        //url: "https://fernanda15func.azurewebsites.net/api/PostRequestFunction",
+        url: "http://localhost:7071/api/PostRequestFunction",        
         data: {
             name: $('#form-input-name').val()
         },
-        type: "POST",
-        dataType: "html",
+        beforeSend: function(request) {
+            request.setRequestHeader('Host', 'https://www.fernandafaz15.website');
+            request.setRequestHeader('Content-Length', 100);
+            request.setRequestHeader('Content-Type', 'application/json');
+        },
+        type: "POST",        
+        dataType: "json",
         success: function (data) {
             $('.js-form').css('display', 'none');
             $('.js-form-success-result').css('display', 'block');
